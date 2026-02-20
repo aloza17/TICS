cabd6667c9eb5d49e7b6ad0b278f09f41aa4fba4
Ashley Loza Velazquez <aloza1@ucol.mx>
2026-02-20 16:40:51 -0600

    Agregar contactos

--- agenda/js/agenda.js
+++ agenda/js/agenda.js
@@ -1,30 +1,63 @@
-document.write("Una agenda")
-let opcion =parseInt (prompt("Escoge una opcion: \n 1:Agregar \n 2:Listar \n 3:Salir"));
-//lo recibe en cadena gracias al prompt
-let seguir= true;
+let seguir = true;
 let datos = [];
 
+while(seguir){
+    let opcion = parseInt(prompt(
+        "Bienvenid@s a tu agenda \n" +
+        "1. Agregar contacto \n" +
+        "2. Listar contactos \n" +
+        "3. Eliminar contactos \n" +
+        "4. Salir"
+    ));
 
-while(seguir) {
-let opcion =parseInt (prompt("Escoge una opcion: \n 1:Agregar \n 2:Listar \n 3:Salir"));
-switch(opcion) {
-    case 1: 
-        alert("Elegiste Agregar");
-        let nombre = prompt("Ingresa tu nombre");
-        datos.push(nombre);
-        console.table(datos);
-        break;
-    case 2:
-        alert("Elegiste Listar");
-        break;
-    case 3:
-        alert("Elegiste Salir");
-        seguir = false;
-        break;
+    switch(opcion) {
+        case 1:
+            let nombre = prompt("Ingresa tu nombre:");
+            let edad = parseInt(prompt("Ingresa tu edad:"));
+            let telefono = prompt("Ingresa tu telefono:");
+            let id_contacto=datos.length + 1;
+            let agenda = {id_contacto, nombre, edad, telefono };
+            agenda.id_contacto = id_contacto;
+            agenda.nombre = nombre;
+            agenda.edad = edad;
+            agenda.telefono = telefono;
 
-    default:
-        alert("No elegiste una opcion valida")
-        break;
+            datos.push(agenda); //para agregar los datos
+            console.log(datos);
+            alert("Tu contacto fue agregado correctamente");
+            break;
+        case 2:
+            let todos = "";
+            for(let i = 0; i < datos.length; i++){
+                todos += "Nombre: " + datos[i].nombre + "\n";
+                todos += "Edad: " + datos[i].edad + "\n";
+                todos += "Telefono: " + datos[i].telefono + "\n";
+                todos += "-----------------------\n";
+            }
 
-}
+            if(datos.length === 0){
+                alert("No hay contactos registrados");
+            } else {
+                alert(todos);
+                console.table(datos);
+            }
+            break;
+        case 3:
+            let id_eliminar = parseInt(prompt("Ingresa el ID del contacto que deseas eliminar"));
+            let index_registrado = datos.findIndex(contacto => contacto.id_contacto === id_eliminar);
+
+            if(index_registrado !== -1){
+                datos.splice(index_registrado, 1);
+                alert("Contacto eliminado correctamente");
+            } else {
+                alert("No se encontró un contacto con ese ID");
+            }    
+        case 4:
+            if(confirm(" Quieres cerrar sesión?")){
+                seguir = false;
+            }
+            break;
+        default:
+            alert("Error");
+    }
 }
